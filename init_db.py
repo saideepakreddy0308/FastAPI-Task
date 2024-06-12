c0701a44e84663121a0bc0d70260be2d35d33f34
Deepak - Antino Labs <deepak.r@antino.com>
2024-06-12 17:20:37 +0530

    database initialize and tables creation

---
+++ init_db.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base
+
+DATABASE_URL = "postgresql://postgres:deepak@localhost/bookstore"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
